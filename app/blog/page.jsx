0175b5671b78c513d39451ef65ee4cf8f9158a70
amir-mansoor <qazimansor89@gmail.com>
2025-11-07 13:47:43 +0500

    /blog error fixed

--- app/blog/page.jsx
+++ app/blog/page.jsx
@@ -1,7 +1,8 @@
-import axios from "axios";
 import BlogScreen from "./BlogScreen";
 export default async function BlogPage() {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/blog`, {});
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/blog`, {
+    cache: "no-cache",
+  });
   const blogs = await res.json();
 
   return <BlogScreen blogs={blogs} />;
